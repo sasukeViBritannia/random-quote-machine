32be0aa4273ff919bd6093621ab1f581f790fd7f
Ale <ale@aleLinux>
2017-04-20 21:55:24 +0200

    Completata animazione nuova cit

--- animazione.js
+++ animazione.js
@@ -1,6 +1,31 @@
 "use strict";
 $(document).ready(function(){
+
+	var arrCita = [
+		["balblblblblblblblblblblblblblblbllblblb", "alice"],
+		["cacacacacacacaacaacacacacacacacacacacac", "bob"],
+		["wfwfwfwfwfwfwwfwfwfwfwwfwfwfwfwfwfwfwfw", "carol"]
+	];
+
+	//var l = arrCita.length;
+
+
 	$("#nextCit").click(function(){
-		$("#citazione").text("Testo cambiato!!");
+
+		var l = arrCita.length;
+		if(l == 0){
+			alert("Citazioni terminate!!");
+		}
+		else {
+			var indice = (Math.floor((Math.random()*10)))%arrCita.length;
+			var testo = arrCita[indice][0];
+			var autore = arrCita[indice][1];
+			alert("Autore = "+autore);
+			arrCita.splice(indice, 1);
+
+			$("#citazione").text(testo);
+			$("#autore").text(autore);
+		}
+
 	});
 });
