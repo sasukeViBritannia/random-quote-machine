53c59dbb5fee3d83ee9e6b79afde174e6e42fa0e
ale <ale@ubuntu>
2017-04-27 16:31:22 +0200

    Aggiunto pulsante di condivisone Twitter

--- animazione.js
+++ animazione.js
@@ -1,31 +1,30 @@
 "use strict";
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var arrCita = [
-		["balblblblblblblblblblblblblblblbllblblb", "alice"],
-		["cacacacacacacaacaacacacacacacacacacacac", "bob"],
-		["wfwfwfwfwfwfwwfwfwfwfwwfwfwfwfwfwfwfwfw", "carol"]
-	];
+    var arrCita = [
+        ["balblblblblblblblblblblblblblblbllblblb", "alice"],
+        ["cacacacacacacaacaacacacacacacacacacacac", "bob"],
+        ["wfwfwfwfwfwfwwfwfwfwfwwfwfwfwfwfwfwfwfw", "carol"]
+    ];
 
-	//var l = arrCita.length;
+    //var l = arrCita.length;
 
 
-	$("#nextCit").click(function(){
+    $("#nextCit").click(function() {
 
-		var l = arrCita.length;
-		if(l == 0){
-			alert("Citazioni terminate!!");
-		}
-		else {
-			var indice = (Math.floor((Math.random()*10)))%arrCita.length;
-			var testo = arrCita[indice][0];
-			var autore = arrCita[indice][1];
-			alert("Autore = "+autore);
-			arrCita.splice(indice, 1);
+        var l = arrCita.length;
+        if (l === 0) {
+            window.alert("Citazioni terminate!!");
+        } else {
+            var indice = (Math.floor((Math.random() * 10))) % arrCita.length;
+            var testo = arrCita[indice][0];
+            var autore = arrCita[indice][1];
+            window.alert("Autore = " + autore);
+            arrCita.splice(indice, 1);
 
-			$("#citazione").text(testo);
-			$("#autore").text(autore);
-		}
+            $("#citazione").text(testo);
+            $("#autore").text(autore);
+        }
 
-	});
+    });
 });
