d5d913950546ec06e7130121154b7b98a946f540
Ale <ale@aleLinux>
2017-04-29 22:38:27 +0200

    Sistemato layout responsivo ed aggiuntee citazioni

--- animazione.js
+++ animazione.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
 
 	/*array contenente le citazioni */
     var arrCita = [
-        ["balblblblblblblblblblblblblblblbllblblb", "alice"],
-        ["cacacacacacacaacaacacacacacacacacacacac", "bob"],
-        ["wfwfwfwfwfwfwwfwfwfwfwwfwfwfwfwfwfwfwfw", "carol"]
+    ['Nulla si crea, nulla si distrugge, tutto si trasforma.', 'Antoine Lavoisier'],
+    ['La mia libertà finisce quando inizia la vostra.', 'Martin Luther King'],
+    ['Dai diamanti non nasce niente, dal letame nascono i fiori.', 'Fabrizio De Andre'],
+    ['Qualsiasi cosa tu faccia sarà insignificante, ma è molto importante che tu la faccia.', 'Gandhi'],
+    ["Uccidete pure me, ma l'idea che è in me non l'ucciderete mai.", 'Giacomo Matteotti'],
+    ['Rigore è quando arbitro fischia.', 'Vujadin Boskov'],
+    ['Veni, vidi, vici.', 'Giulio Cesare'],
+    ['La mente p come un paracadute, funziona solo se si apre.', 'Albert Einstein'],
+    ['La verità si ritrova sempre nella semplicità, mai nella confusione.', 'Isaac Asimov'],
+    ["Dove c'è una grande volontà non possono esserci grandi difficoltà.", "Niccolò Macchiavelli"]
     ];
 
     $("#nextCit").click(function() {
@@ -23,7 +30,7 @@ $(document).ready(function() {
 
             $("#citazione").text(testo);
             $("#autore").text(autore);
-            $(this).parent().prev().find('span').text(4-l);
+            $(this).parent().prev().find('span').text(11-l);
             $('#shareTwitter').attr({ 'href': 'https://twitter.com/intent/tweet?text=' + testo, 'target': '_blank' });
 
         }
